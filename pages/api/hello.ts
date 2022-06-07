3f1fdadfade6d14d4983f3407e18dca38e579bbd
nivedin <nivedinp@gmail.com>
2022-06-07 20:59:55 +0530

    fix: check remove

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,18 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
-  id:{
-    name: any
-    age: any
-  }
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe',id:{
-    name:process.env.NEXT_PUBLIC_CLIENT_ID,
-    age:process.env.NEXT_PUBLIC_CLIENT_SECRET
-  } })
+  res.status(200).json({ name: 'John Doe'})
 }
