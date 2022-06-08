59d0c200667112827871c03bf330320de78bbfb3
nivedin <nivedinp@gmail.com>
2022-06-08 16:14:26 +0530

    refactor: improve dashboard UI

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,10 @@
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 import PrimaryButton from "../components/buttons/PrimaryButton";
 import SecondaryButton from "../components/buttons/SecondaryButton";
 import UserLayout from "../components/user/UserLayout";
+import dashBannerImage from "../public/images/dashboard/illus1.png";
 
 type Props = {};
 
@@ -13,7 +15,7 @@ function dashboard({}: Props) {
   return (
     <UserLayout>
       {session && (
-        <div className="container py-10 px-5">
+        <div className="w-full py-10 px-5 sm:px-10">
           <div>
             <div>
               <div>
@@ -21,20 +23,36 @@ function dashboard({}: Props) {
                 <h2 className="text-3xl font-bold">Welcome Back !</h2>
               </div>
             </div>
-            <div className="flex items-center bg-accentDark min-h-40vh my-10 rounded-xl p-5">
-              <div className="flex items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center bg-accentDark min-h-40vh my-10 rounded-xl ">
+              <div className="flex items-center py-5 px-10">
                 <div className="flex flex-col">
-                  <h3 className="text-sm">Reminder</h3>
-                  <h3 className="text-2xl max-w-50">
+                  <h3 className="text-sm md:text-md xl:text-xl text-accentContrast">
+                    Reminder
+                  </h3>
+                  <h3 className="text-2xl text-accentContrast sm:text-4xl 2xl:text-6xl xl:max-w-200 max-w-80">
                     Have you Had a Routine Check this Month ?
                   </h3>
                   <div className="flex space-x-5 my-5">
-                    <PrimaryButton label="Check Now" />
-                    <SecondaryButton label="View Report" />
+                    <PrimaryButton
+                      label="Check Now"
+                      className="text-accentContrast"
+                    />
+                    <SecondaryButton
+                      label="View Report"
+                      className="text-accentContrast"
+                    />
                   </div>
                 </div>
               </div>
-              <div></div>
+              <div className="relative h-full hidden md:block">
+                <Image
+                  src={dashBannerImage}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="Doctors image"
+                  placeholder="blur"
+                />
+              </div>
             </div>
           </div>
         </div>
